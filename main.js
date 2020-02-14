83f27f22820c705ec53b83a270a505b886f7a8ab
Darkil-HS <n00049623@upn.pe>
2020-02-14 07:39:39 -0500

    v6.5

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ let nav = document.getElementById('nav-');
 function menus(){
     let des_actual = window.pageYOffset;
 
-    if(des_actual <= 250){
+    if(des_actual <= 100){
         nav.classList.remove = 'nav-2';
         nav.className = 'nav';
         nav.style.transition = '1s';
     }
     else{
         nav.className = 'nav nav-2';
-        nav.style.transition = '1s';
+        nav.style.transition = '0.7s';
     }
 }
 
