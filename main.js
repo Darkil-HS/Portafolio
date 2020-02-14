651c0c467fdaad9ac9a6a48c4cdd7cf4fcd3b1b6
Darkil-HS <n00049623@upn.pe>
2020-02-14 04:16:29 -0500

    v5

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-$(function(){alert();});
+// variables
+let nav = document.getElementById('nav-');
+
+function menus(){
+    let des_actual = window.pageYOffset;
+
+    if(des_actual <= 250){
+        nav.classList.remove = 'nav-2';
+        nav.className = 'nav';
+        nav.style.transition = '1s';
+    }
+    else{
+        nav.className = 'nav nav-2';
+        nav.style.transition = '1s';
+    }
+}
+
+window.addEventListener('scroll',function(){
+    menus();
+    
+});
+
+window.addEventListener('click',function(e){
+    console.log(e.target);
+});
+
